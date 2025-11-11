6840fbef5f1498eb2c93da3a6411db6ccbd0f89d
Zeeshan Iqbal <zeeshan.iqbal.cs@gmail.com>
2025-11-11 17:07:50 +0500

    setup: project with the nest-js and prisma with "prisma-client-js"

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+
+import { DATABASE_CONNECTION_FAILED } from 'src/error-codes.consts';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
+  async onModuleInit() {
+    try {
+      this.logger.log('Connecting to database ...');
+      await this.$connect();
+      this.logger.log('Connection established with the database.');
+    } catch (error) {
+      this.logger.error('Prisma connection failed', error);
+
+      process.exit(DATABASE_CONNECTION_FAILED);
+    }
+  }
+}
