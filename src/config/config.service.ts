d49a860f4f4ccb7f461daacb67cb4d06d3c30e5e
Zeeshan Iqbal <zeeshan.iqbal.cs@gmail.com>
2026-01-08 11:41:58 +0500

    fix: config

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -4,7 +4,11 @@ import { CLERK_SECRET_KEY, CLERK_PUBLISHABLE_KEY } from 'src/app.consts';
 
 @Injectable()
 export class AppConfigService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {
+    const v = configService.getOrThrow(null, { infer: true });
+
+    console.log(v);
+  }
 
   getClerkSecretKey(): string {
     return this.configService.getOrThrow<string>(CLERK_SECRET_KEY);
